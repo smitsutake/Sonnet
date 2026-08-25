fae333dc6017dec34a632601707526f4ebc7a595
chenyanzhe <yanzhe@student.unimelb.edu.au>
2026-08-26 06:17:18 +1000

    feat: replace Papers page with modal overlay in welcome page

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,12 +1,16 @@
 import React, {useState} from "react";
-
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
 import WelcomeHeader from "./WelcomeHeader";
 import WelcomeFooter from "./WelcomeFooter";
 import WelcomeButtons from "./WelcomeButtons";
 import {DetailCarousel} from "./DetailCarousel";
+import PaperReferenceList from "./PaperReferenceList";
+import {papers} from "../data/papers";
 
 const Welcome = () => {
 	const [isDragging, setIsDragging] = useState(false);
+	const [showPapers, setShowPapers] = useState(false);
 
 	// Handle dragging files to upload
 	const handleDragOver = (evt: React.DragEvent<HTMLDivElement>) => {
@@ -26,7 +30,22 @@ const Welcome = () => {
 				<DetailCarousel />
 				<WelcomeButtons isDragging={isDragging} setIsDragging={setIsDragging} />
 			</div>
-			<WelcomeFooter destination="papers" name="Papers" />
+			<WelcomeFooter onPapersClick={() => setShowPapers(true)} />
+
+			{/* Papers Modal */}
+			<Modal show={showPapers} onHide={() => setShowPapers(false)} size="lg" centered>
+				<Modal.Header closeButton>
+					<Modal.Title>Papers &amp; User Manual</Modal.Title>
+				</Modal.Header>
+				<Modal.Body>
+					<PaperReferenceList references={papers} />
+				</Modal.Body>
+				<Modal.Footer>
+					<Button variant="secondary" onClick={() => setShowPapers(false)}>
+						Close
+					</Button>
+				</Modal.Footer>
+			</Modal>
 		</div>
 	);
 };
