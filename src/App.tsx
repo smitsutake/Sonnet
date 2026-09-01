71f91c278bb8669d887af6cee63b224709409c42
YilanFang <yilanf@student.unimelb.edu.au>
2026-09-02 03:52:57 +1000

    user-manual web base on origin usermanual pdf

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import Welcome from "./components/Welcome";
 import Papers from "./components/Papers";
 import ProjectEdit from "./components/ProjectEdit";
 import Layout from "./components/Layout";
+import UserManualWeb from "./UserManual/UserManualWeb";  // new add
+
 
 const App = () => {
 	return (
@@ -12,6 +14,8 @@ const App = () => {
 					<Route path="/" element={<Welcome />} />
 					<Route path="/papers" element={<Papers />} />
 					<Route path="/projectEdit" element={<ProjectEdit />} />
+					// new add
+					<Route path="/user-manual-web" element={<UserManualWeb />} />
 				</Routes>
 			</Layout>
 		</BrowserRouter>
--- src/components/WelcomeFooter.tsx
+++ src/components/WelcomeFooter.tsx
@@ -1,5 +1,9 @@
 import {Link} from "react-router-dom";
-
+//new add
+import { useState } from "react";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+//
 type WelcomeFooterProps = {
 	name: string;
 	destination: string;
@@ -7,23 +11,58 @@ type WelcomeFooterProps = {
 
 // Footer section in welcome page including copyrights, papers, user manual...
 const WelcomeFooter = ({name, destination}: WelcomeFooterProps) => {
+	// new add
+	const [showModal, setShowModal] = useState(false);
+
+	const handleWebView = () => {
+		setShowModal(false);
+		window.open("/mm-local-editor/user-manual-web", "_blank");
+	};
+
+	const handlePdfView = () => {
+		setShowModal(false);
+		window.open("/mm-local-editor/AMMBER_User_Manual.pdf", "_blank");
+	};
+
+	//
 	return (
-		<div className="text-center mt-auto">
-			<p>
-				<Link to={`/${destination}`} className="text-decoration-none">
-					{name}
-				</Link>
-				&nbsp;|&nbsp;
-				<Link
-					to="/papers/AMMBER_User_Manual.pdf"
-					target="_blank"
-					className="text-decoration-none"
-				>
+		<>
+			<div className="text-center mt-auto">
+				<p>
+					<Link to={`/${destination}`} className="text-decoration-none">
+						{name}
+					</Link>
+					&nbsp;|&nbsp;
+					{/* new add: change Link to span  */}
+					<span
+						onClick={() => setShowModal(true)}
+						style={{cursor: "pointer", color: "#0d6efd", textDecoration: "underline"}}
+					>
 					User Manual
-				</Link>
-			</p>
-		</div>
+				</span>
+				</p>
+			</div>
+
+			{/* new add: Added pop-up modal  */}
+			<Modal show={showModal} onHide={() => setShowModal(false)} centered>
+				<Modal.Header closeButton>
+					<Modal.Title>View User Manual</Modal.Title>
+				</Modal.Header>
+				<Modal.Body>
+					<p>Please choose how you would like to view the user manual:</p>
+				</Modal.Body>
+				<Modal.Footer>
+					<Button variant="outline-secondary" onClick={handlePdfView}>
+						📄 PDF View
+					</Button>
+					<Button variant="primary" onClick={handleWebView}>
+						🌐 Web View
+					</Button>
+				</Modal.Footer>
+			</Modal>
+		</>
 	);
 };
+
 
 export default WelcomeFooter;
